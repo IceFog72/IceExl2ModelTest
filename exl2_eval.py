aa0b4ddb0c8f744b937083e75e106a6e35da1a99
IceFog72 <164350516+IceFog72@users.noreply.github.com>
2024-07-07 16:51:23 +0300

    Update exl2_eval.py

--- exl2_eval.py
+++ exl2_eval.py
@@ -45,6 +45,22 @@ qa_split = "test"
 
 mmlu_categories = ["abstract_algebra", "formal_logic", "logical_fallacies", "philosophy"]
 
+# mmlu_categories = [
+#     "abstract_algebra", "anatomy", "astronomy", "business_ethics", "clinical_knowledge",
+#     "college_biology", "college_chemistry", "college_computer_science", "college_mathematics",
+#     "college_medicine", "college_physics", "computer_security", "conceptual_physics", 
+#     "econometrics", "electrical_engineering", "elementary_mathematics", "formal_logic",
+#     "global_facts", "high_school_biology", "high_school_chemistry", "high_school_computer_science",
+#     "high_school_european_history", "high_school_geography", "high_school_government_and_politics",
+#     "high_school_macroeconomics", "high_school_mathematics", "high_school_microeconomics",
+#     "high_school_physics", "high_school_psychology", "high_school_statistics", "high_school_us_history",
+#     "high_school_world_history", "human_aging", "human_sexuality", "international_law", 
+#     "jurisprudence", "logical_fallacies", "machine_learning", "management", "marketing",
+#     "medical_genetics", "miscellaneous", "moral_disputes", "moral_scenarios", "nutrition",
+#     "philosophy", "prehistory", "professional_accounting", "professional_law", "professional_medicine",
+#     "public_relations", "security_studies", "sociology", "us_foreign_policy", "virology", "world_religions"
+# ]
+
 mmlu_questions_per_category_count = 100
 winogrande_questions_count = 100
 musr_questions_count = 100
